367ad314f213bd182b9d9a67be5ffd068f23fbb1
SAHIL-HANSA <106074469+SAHIL-HANSA@users.noreply.github.com>
2023-08-26 17:32:53 +0530

    Add files via upload

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,16 @@
+const { ethers } = require("hardhat");
+ 
+async function main() {
+  const Contract = await ethers.getContractFactory("DegenGamingToken"); // Replace "DegenGamingToken" with the actual contract name
+  const contract = await Contract.deploy(initialSupply); // Replace "initialSupply" with the desired initial supply for the token
+  await contract.deployed();
 
+  console.log("Contract deployed to:", contract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
